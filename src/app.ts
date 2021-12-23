95a172550148c673ed3edf38326cbd08d8568860
khronic79 <rustam.ptashinskiy@gmail.com>
2021-12-23 23:02:09 +0300

    feat: add logger

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import path from 'path';
 import YAML from 'yamljs';
 import { koaSwagger } from 'koa2-swagger-ui';
 import json from 'koa-json';
+import { createLogger, format, transports } from 'winston';
 import { router as userRouter } from './resources/users/user.router';
 import { router as boardRouter } from './resources/boards/board.router';
 import { router as taskRouter } from './resources/tasks/task.router';
@@ -23,6 +24,19 @@ router.get('/', (ctx, next) => {
 });
 
 app
+  .use(async (ctx, next) => {
+    await next();
+    const logger = createLogger({
+      format: format.combine(
+        format.splat(),
+        format.simple(),
+        format.timestamp(),
+        format.prettyPrint(),
+      ),
+      transports: [new transports.Console()]
+    });
+    logger.log('error', 'test message %s', ctx.body);
+  })
   .use(bodyParser())
   .use(json())
   .use(router.routes())
