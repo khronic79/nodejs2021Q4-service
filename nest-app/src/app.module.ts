3f7013a9832ef81ac5738fe55db41161c149f775
khronic79 <rustam.ptashinskiy@gmail.com>
2022-01-24 00:54:32 +0300

    feat: create main modules

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TasksModule, BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
